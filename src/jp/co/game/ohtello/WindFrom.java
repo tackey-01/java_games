c23aa530bbf34e34b241e7d962b19b9bd30c3682
SHIN02\takinoshin <shin1989takino@gmail.com>
2019-08-03 22:20:29 +0900

    create windFrom

---
+++ src/jp/co/game/ohtello/WindFrom.java
@@ -0,0 +1,56 @@
+package jp.co.game.ohtello;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class WindFrom extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WindFrom frame = new WindFrom();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public WindFrom() {
+		setTitle("OhtelloGame");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 640, 640);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+
+		JPanel text_panel = new JPanel();
+		JLabel text_label = new JLabel();
+		text_label.setText("OhelloGame");
+		text_panel.add(text_label);
+
+		Container contentPane = getContentPane();
+		System.out.println(contentPane.getColorModel());
+	    contentPane.add(text_panel, BorderLayout.LINE_START);
+
+	}
+
+}
